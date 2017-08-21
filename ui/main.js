da4b57e3f6d03264cf6f737b34caafb8aeceb5af
Kaushik Y H <30903784+Kaushik066@users.noreply.github.com>
2017-08-21 21:09:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
-console.log('Loaded!');
+//console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter =0;
+
+button.onclick = function(){
+    
+    //make request to end-point
+    var request= new XMLHttpRequest();
+    
+    //capture the responce and store it in the a variable
+    
+    //render the variable in correct span
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
