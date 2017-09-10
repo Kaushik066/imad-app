60db2bfce603e050f40aeeed8ec5b6f65d468412
Kaushik Y H <30903784+Kaushik066@users.noreply.github.com>
2017-09-10 13:49:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var config = {
 
 var app=express();
 app.use(morgan('combined'));
-app.use(bodyparser.JSON);
+app.use(bodyParser.JSON);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
