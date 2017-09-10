0a41e2458f037e58a2c55774f6dca0cfa880d615
Kaushik Y H <30903784+Kaushik066@users.noreply.github.com>
2017-09-10 13:31:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ var path = require('path');
 var counter=0;
 var app = express();
 var crypto = require('crypto');
+var pool = requir('pg').Pool;
 
+var bodyParser = requir('body-parser');
+
+var app=express();
 app.use(morgan('combined'));
+app.use(bodyparser.JSON);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -20,6 +25,35 @@ app.get('/hash/:input',function(req,res){
     var hashedString = hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
 });
+
+app.post('/cresate-user', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString =hash(password,salt); 
+    pool.querry('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString], function(err,result){
+        if(err){
+        res.status(500).send(err.toString());
+          
+      } else{
+          res.send('User successfully created '+ username);
+          
+      }
+  });
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res){
+    pool.querry('SELECT * FROM test', function(err,result){
+      if(err){
+        res.status(500).send(err.toString());
+          
+      } else{
+          res.send(JSON.stringify(result.rows));
+      }
+});
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -44,10 +78,10 @@ app.get('/counter',function(req,res){
   counter = counter + 1;
   res.send(counter.toString());
 });
+
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-
-
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
