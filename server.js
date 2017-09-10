78fd0950f1f3ad12eca7943165c9d779309e1fbf
Kaushik Y H <30903784+Kaushik066@users.noreply.github.com>
2017-09-10 06:30:28 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/articles/articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
    pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName +"'",function(err,result){
       if(err){
         res.status(500).send(err.toString());
@@ -81,6 +81,7 @@ app.get('/articles/articleName',function(req,res){
           res.status(404).send('Article not found');
         }
         else{
+            var articleData = result.rows[0];
             res.send(createTemplate(articleData));
         }
       }
