7cf96cacad93674a81ce70d44ee67abc74b06ea5
Kaushik Y H <30903784+Kaushik066@users.noreply.github.com>
2017-09-10 14:34:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,31 @@ app.get('/counter',function(req,res){
   res.send(counter.toString());
 });
 
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>${title}</tile>
+            <meta name="viewport" content="width-device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div><a href="/">Home</a></div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>${date.toDateString()}</div>
+                <div>${content}</div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
